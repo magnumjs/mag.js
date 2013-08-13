352f5b9b56d9a34ce1d6d9025e9d2a26b0f04c91
magnumjs <magnumjs@users.noreply.github.com>
2013-08-12 22:22:40 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+//////////////////////////////////
+//MagJS
+//////////////////////////////////
+/*!
+ * MagnumJS - Core Control Factory v0.11.1
+ * https://github.com/magnumjs
+ *
+ * Includes Staples.js & Glue.JS
+ * https://github.com/magnumjs/mag.js
+ *
+ * Copyright 2013 Michael GLazer
+ * Released under the MIT license
+ * https://github.com/magnumjs/mag.js/blob/master/LICENSE
+ *
+ * Date: 2013-08-10T13:48Z
+ */
 // Code goes here
 var todosControl = function ($scope) {
     //$scope.test= 'Yo!';
@@ -19,16 +35,33 @@ var todosControl = function ($scope) {
         return $scope.todos.length;
     };
 }
+mag.observe().on('tmpl-begin', function (name) {
+    $('#' + name).hide();
+});
+mag.observe().on('tmpl-end', function (name) {
+    if (name == 'todosControl') {
+        $('#' + name).show();
+
+        var ctrl = new mag.watch();
+        ctrl.secret = 'null';
+        ctrl._bind($('#todoText'), 'secret');
+        ctrl._watch($('.output'), 'secret');
+
+        // mag.observe().on('propertyChanged',function(prop,val){
+        //   console.log(prop+'='+val);
+        // });
+    }
+});
 
 mag.control('todosControl', ['test', todosControl]);
 $('.todos').addClass('more');
-//$('.todos input').after('[[test]]');
+//$('.todos input').after('="[[test]]"');
 setTimeout(function () {
     mag.control('todosControl', ['',
         function ($scope) {
-            $scope.test = 'momma';
+            // $scope.test='momma';
             $scope.todos[0].done = false;
         }
     ]);
-    console.log($('.todos:eq(0) span').attr('class'));
+
 }, 1000);
