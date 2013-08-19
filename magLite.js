367aa69ebe18f9a735531c3f1482761781643e4c
magnumjs <magnumjs@users.noreply.github.com>
2013-08-18 20:02:15 -0400

    Update magLite.js
    Added AOP

--- magLite.js
+++ magLite.js
@@ -6,7 +6,65 @@
 */
 
 mag={};
-mag.module=function(name){
+
+mag.aspect={
+  injectors:[],
+  namespace:null,
+  add: function(type,name,fun){
+    mag.aspect.injectors.push({type:type,name:name,fun:fun});
+  },
+  attach:function(){
+    for(var i in mag.aspect.injectors){
+      var inject=mag.aspect.injectors[i];
+      if( mag.aspect[inject.type]){
+        mag.aspect[inject.type](inject.name,inject.fun); 
+      }
+    }
+  },
+  around: function(pointcut, advice) {
+
+ var ns = mag.aspect.namespace;    
+    for(var member in mag.aspect.namespace) {
+
+        if(typeof ns[member] == 'function' && member.match(pointcut)) {
+         
+          (function(fn, fnName, ns) {
+  
+             ns[fnName] = function() {
+               return advice.call(ns, { fn: fn,
+                                          fnName: fnName,
+                                          arguments: arguments });
+             };
+           })(ns[member], member, ns);
+        }
+      }
+    
+  },
+  before : function(pointcut, advice) {
+  mag.aspect.around(pointcut,
+             function(f) {
+               advice.apply(this.namespace, f.arguments);
+               return mag.aspect.next(f)
+             });
+},
+  after:function(pointcut,advice) {
+  mag.aspect.around(pointcut,
+             function(f) {
+               var ret = mag.aspect.next(f);
+               advice.apply(this.namespace, f.arguments);
+               return ret;
+             });
+},
+  next: function(f) {
+    return f.fn.apply(this.namespace, f.arguments);
+  }
+};
+
+mag.inject=function(ns){
+  mag.aspect.namespace=ns;
+  mag.aspect.attach();
+}
+mag.module=function(name){  
  var Injector = {
     
     dependencies: {},
@@ -36,6 +94,7 @@ mag.module=function(name){
 
 };
   return new function(){
+ 
   this.name=name;
   this.service=function(name,fun){
     this.services=this.services||{};
@@ -64,13 +123,23 @@ mag.module=function(name){
             }
         }
     }
+     mag.inject(this);
   }
+  
 }
 
 /*
-var app = mag.module('myApp');
+  
+mag.template=function(f) {
+ console.log('[before ' + f.fnName + ']');
+ mag.aspect.next(f);
+ console.log('[after ' + f.fnName + ']');
+ }; 
 
-console.log(app);
+
+mag.aspect.add('around','control',mag.template);
+
+var app = mag.module('myApp');
 
 // singleton object instance "new"
 app.service('Api',function(){
@@ -79,8 +148,11 @@ app.service('Api',function(){
   }
 });
 
-app.control('myCtrl',function(Scope,Api){
-       Scope.list=Api.getProjects();  
+
+app.control('myCtrl',function(Scope){
+  Scope.test='Yo';
+});
+app.control('myCtrl',function(Scope){
   console.log(Scope);
 });
 */
