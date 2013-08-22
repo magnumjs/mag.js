61864bf6486c9a6a691a1fcb58ce5b93cc23123d
magnumjs <magnumjs@users.noreply.github.com>
2013-08-22 17:11:37 -0400

    Update magLite.js
    giving factory it's own context

--- magLite.js
+++ magLite.js
@@ -124,8 +124,8 @@ mag.module = function (name) {
         }
         this.factory = function (name, fun) {
             this.factories = this.factories || {};
-            this.factories[name] =this.factories[name]|| fun();
-            Injector.register(name, this.factories[name]);
+            this.factories[name] =this.factories[name]|| {};
+            Injector.register(name, fun.call(this.factories[name]));
         }
         this.control = function (name, fun) {
             this.controls = this.controls || {};
