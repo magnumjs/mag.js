a9b1e1a4204fbbbce647574a713adba3a57b97c2
magnumjs <magnumjs@users.noreply.github.com>
2014-04-07 16:12:14 -0400

    Update magLite.js

--- magLite.js
+++ magLite.js
@@ -177,7 +177,7 @@ mag.module = function (name) {
             this.fire('mag-postload', [name]);
         }
         this.getScope = function (name) {
-            return this.controls[name] = this.controls[name]?this.controls[name]['__instance']:delete this.controls[name]['__instance'] || {};
+            return this.controls[name] = this.controls[name] || {};
         }
         this.observers = {};
         this.on = function (eventNames, listener) {
