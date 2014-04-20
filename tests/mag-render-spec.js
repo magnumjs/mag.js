e34ebb3ec472f58498760af8891f988a0f03063e
magnumjs <magnumjs@users.noreply.github.com>
2014-04-20 09:01:34 -0400

    Update and rename mag-template-spec.js to mag-render-spec.js

--- tests/mag-render-spec.js
+++ tests/mag-render-spec.js
@@ -15,7 +15,7 @@ $scope.todos = [{
 }];
 describe("MagJS Template", function() {
   it("is defined", function() {
-    expect(mag.template).toBeDefined();
+    expect(mag.render).toBeDefined();
   });
   describe("when called", function() {
     var $html;
@@ -31,6 +31,15 @@ describe("MagJS Template", function() {
       var app3 = mag.module('appOther');
       expect(app1).not.toEqual(app3);
     });
+    it("won't run non existent controller container", function() {
+      var $html = affix('#test span');
+      $html.find('span').text('[[name]]');
+      var app = mag.module('appTest');
+      app.control('testFake', function(Scope) {
+        Scope.name = 'Joe';
+      });
+      expect($html.html()).not.toHaveText('Joe');
+    });
     it("config a factory", function() {
       //affix('#test');
       var app = mag.module('app');
@@ -45,6 +54,20 @@ describe("MagJS Template", function() {
       });
 
       expect($html.html()).toEqual('<div class="id">test</div>');
+    });
+    it("uiEvents module 1", function() {
+      $html = affix('#ctrl .id button[data-event-click="add"]+.test');
+      $html.find('.test').text('[[othery]]');
+      var event = mag.module('click');
+      event.control('ctrl', function(Scope) {
+        Scope.other = 'test';
+        Scope.add = function() {
+          Scope.othery = this.other;
+          //console.log(Scope);
+        };
+      });
+      $('button').click();
+      expect($html.find('.test').text()).toEqual('test');
     });
     it("uiEvents module", function() {
       $html = affix('#ctrl .id button[data-event="add"]+.test');
@@ -84,23 +107,35 @@ describe("MagJS Template", function() {
       //     expect(Scope.test).toEqual('Yo!');
       //   }
       // ]);
-      console.log($html.html());
+      //console.log($html.html());
       expect($html.find('.test')).toHaveText('Yo! 1');
     });
-    it("parses control id node with Scope data", function() {
+    it("change scope in reusable module", function() {
       expect($html).toContainHtml('[[test]]');
       var app = mag.module('myTApp');
       app.control('todosControl',
         function(Scope) {
           Scope.test = 'YoYo!';
         });
-      expect($html).toContainText('YoYo!');
-      expect($html.find('.message')).toHaveLength(3);
-      expect($html.find('.message').eq(0)).toHaveClass('completed-true');
-      expect($html.find('.message').eq(1)).toHaveClass('completed-false');
-      expect($html.find('.todos')).toHaveLength(3);
-      expect($html).toContainHtml('<div id="todosControl"');
+      expect($html.find('.test').text()).toEqual('YoYo! 1');
+
+    });
+    it("parses control id node with Scope data", function() {
+      expect($html).toContainHtml('[[test]]');
+      var app = mag.module('myTApp', []);
+      app.control('todosControl',
+        function(Scope) {
+          Scope.remaining = function() {
+            return 1;
+          };
+          Scope.test = 'YoYo!';
+        });
+      expect($html.find('.test').text()).toEqual('YoYo! 1');
+      // expect($html.find('.message')).toHaveLength(3);
+      // expect($html.find('.message').eq(0)).toHaveClass('completed-true');
+      // expect($html.find('.message').eq(1)).toHaveClass('completed-false');
+      // expect($html.find('.todos')).toHaveLength(3);
+      // expect($html).toContainHtml('<div id="todosControl"');
     });
   });
-});;
 });
