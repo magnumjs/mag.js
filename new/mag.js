09b4e30594473d25a7655d204618ec5bbc2b4398
magnumjs <magnumjs@users.noreply.github.com>
2015-03-27 09:40:25 -0400

    Update mag.js

--- new/mag.js
+++ new/mag.js
@@ -6,18 +6,27 @@ var mag = (function(self, document, undefined) {
     module = self.mod,
     render = self.render,
     fill = self.fill,
-    watch = mag.watch
+    topModule,
+    watch = mag.watch;
 
-    // REMOVE THIS? why is this here?
-    privates.init = function(container, data) {
+  // REMOVE THIS? why is this here?
+  privates.init = function(container, data) {
 
-      // if (!(this instanceof privates.init)) return new privates.init(container, data)
-      // var instance = this
-      // console.log('mag init')
-    }
+    // if (!(this instanceof privates.init)) return new privates.init(container, data)
+    // var instance = this
+    // console.log('mag init')
+  }
 
   privates.redraw = function() {
     render.redraw(module || render.module || {}, fill, watch)
+  }
+
+  privates.withProp = function(prop, withAttrCallback) {
+    return function(e) {
+      e = e || event;
+      var currentTarget = e.currentTarget || this;
+      withAttrCallback(prop in currentTarget ? currentTarget[prop] : currentTarget.getAttribute(prop))
+    }
   }
 
   privates.prop = function(store) {
@@ -56,7 +65,9 @@ var mag = (function(self, document, undefined) {
     parentElement.replaceChild(tempEle, element)
 
     //MODULE
+    if (!moduleObject.view) throw new Error('view is required')
     var mod = module.submodule(moduleObject, [props])
+
 
     render.roots[index] = domElementId
     var controller = new mod.controller
@@ -69,9 +80,26 @@ var mag = (function(self, document, undefined) {
 
     //DOM
     parentElement.replaceChild(elementClone, tempEle)
+  
+    // call controller unloaders ?
+
 
     // call onload if present in controller
     if (controller.onload) controller.onload.call(null, elementClone)
+
+    // var currentModule = topModule = mod = mod || {};
+    // var constructor = mod.controller || function() {}
+    // var controller = new constructor;
+    // // controller._type='__mag__.module'
+    // // controller._ele = domElementId
+    // //controllers may call m.module recursively (via m.route redirects, for example)
+    // //this conditional ensures only the last recursive m.module call is applied
+    // if (currentModule === topModule) {
+    //   module.controllers[index] = controller;
+    //   module.modules[index] = mod
+    // }
+    
+    return {_type:'__mag__.module', _ele : domElementId}
   }
 
   var interfaces = function(method) {
@@ -85,6 +113,7 @@ var mag = (function(self, document, undefined) {
   api['module'] = interfaces('module')
   api['prop'] = interfaces('prop')
   api['redraw'] = interfaces('redraw')
+  api['withProp'] = interfaces('withProp')
 
   return api
 
