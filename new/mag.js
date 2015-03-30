73dc34b54f5308e472dc21bbbd7356ea6a4a1ad3
magnumjs <magnumjs@users.noreply.github.com>
2015-03-30 11:16:25 -0400

    Update mag.js

--- new/mag.js
+++ new/mag.js
@@ -7,19 +7,21 @@ var mag = (function(self, document, undefined) {
     render = self.render,
     fill = self.fill,
     topModule,
-    watch = mag.watch;
+    watch = self.watch;
+
+  // set logger
+  self.logger = fill.logger || self.logger
 
   // REMOVE THIS? why is this here?
   privates.init = function(container, data) {
-
     // if (!(this instanceof privates.init)) return new privates.init(container, data)
     // var instance = this
-    // console.log('mag init')
   }
   var redrawing = false
   privates.redraw = function() {
     if (redrawing) {
-      console.log('test')
+      // do we ever get here?
+      // necessary?
       return
     }
     redrawing = true
@@ -55,26 +57,17 @@ var mag = (function(self, document, undefined) {
   }
 
 
-  var templates = [],
-    called = [],
-    guid = 0;
-
   privates.module = function(domElementId, moduleObject, props, reuse) {
-    console.time("MagnumJS:init")
+    fill.log('time')("MagnumJS:init:" + domElementId)
 
     var index = render.roots.indexOf(domElementId)
-
-    if (reuse && index !== -1) {
-
-    }
-
     // create new index on roots
     if (index < 0) index = render.roots.length;
 
     //DOM
 
     var element = document.getElementById(domElementId)
-    if (!element) return new Error('invalid node')
+    if (!element) return Error('invalid node')
 
     var parentElement = element.parentNode
     var elementClone = element.cloneNode(true)
@@ -82,25 +75,23 @@ var mag = (function(self, document, undefined) {
     var tempEle = document.createElement("span")
     parentElement.replaceChild(tempEle, element)
 
+    render.roots[index] = elementClone.id
+
+
     //MODULE
-    if (!moduleObject.view) return
+    if (!moduleObject.view) return Error('module requires a view')
 
     var mod = module.submodule(moduleObject, [props])
 
 
-    render.roots[index] = elementClone.id
+    var currentModule = topModule = mod = mod || {}
+    var controller = new mod.controller
 
 
-    var currentModule = topModule = mod = mod || {};
-    var constructor = mod.controller || function() {}
-    var controller = new constructor;
-
-
-    //controllers may call m.module recursively (via m.route redirects, for example)
-    //this conditional ensures only the last recursive m.module call is applied
+    //this conditional ensures only the last recursive module call is applied
+    // REALLY necessary?
     if (currentModule === topModule) {
 
-      //console.log('test', index, render.roots[index])
       module.controllers[index] = controller;
       module.modules[index] = mod
       module.elements[index] = elementClone
@@ -114,20 +105,28 @@ var mag = (function(self, document, undefined) {
     parentElement.replaceChild(elementClone, tempEle)
 
     // call controller unloaders ?
+    // check if was in previous and now not for the same node
+    // add to fill.js
 
 
     // call onload if present in controller
     if (controller.onload) controller.onload.call(null, elementClone)
 
-    console.timeEnd("MagnumJS:init")
+    fill.log('timeEnd')("MagnumJS:init:" + domElementId)
 
+    // return instance ?
+    // module.controllers[index]
     return {
       _html: elementClone.innerHTML
     }
   }
 
-  var interfaces = function(method) {
+  // set to 0/off to disable
+  privates.logger = function(logger) {
+    self.logger = logger
+  }
 
+  var interfaces = function(method) {
     return function() {
       return privates[method].apply(this, arguments)
     }
@@ -139,6 +138,7 @@ var mag = (function(self, document, undefined) {
   api['prop'] = interfaces('prop')
   api['redraw'] = interfaces('redraw')
   api['withProp'] = interfaces('withProp')
+  api['logger'] = interfaces('logger')
 
   return api
 
