b0db4db1c8e3183cc47ba10565b042d8dae6a1eb
magnumjs <magnumjs@users.noreply.github.com>
2015-04-13 19:34:56 -0400

    new filter as component example

---
+++ new/example/filter-component.js
@@ -0,0 +1,94 @@
+/*
+  <div id="dashboard">
+    <div id="filter-users">
+      <h3>Filter users (<span data-bind="selectedUser"><name></name></span>)</h3>
+      <input placeholder="Filter users by ... " />
+      <ul>
+        <li>Person Name: <span data-bind="name"></span>
+        </li>
+      </ul>
+    </div>
+    <div id="filter-projects">
+      <h3>Filter projects (<span data-bind="selectedProject"><name></name></span>)</h3>
+      <input placeholder="Filter projects by ... " />
+      <ul>
+        <li>Person Name: <span data-bind="name"></span>
+        </li>
+      </ul>
+    </div>
+  </div>
+*/
+
+var filter = {
+  controller: function(props) {
+
+    this.term = mag.prop('')
+    this.list = props.list
+    this.binds = props.binds || mag.prop()
+
+    this.search = function(value) {
+      this.term(value.toLowerCase())
+    }.bind(this)
+
+    this.filter = function(item) {
+      return this.term() && item.name.toLowerCase().indexOf(this.term()) > -1
+    }.bind(this)
+
+  },
+  view: function(element, props, state) {
+    state.input = {
+      _oninput: mag.withProp('value', state.search)
+    }
+
+    state.li = state.list.filter(state.filter).map(function(item) {
+      return {
+        _onclick: state.binds.bind(this, item),
+        _text: item.name
+      }
+    })
+  }
+}
+
+var users = [{
+  id: 1,
+  name: "John"
+}, {
+  id: 2,
+  name: "Bob"
+}, {
+  id: 2,
+  name: "Mary"
+}]
+
+var projects = [{
+  id: 1,
+  name: "John's project"
+}, {
+  id: 2,
+  name: "Bob's project"
+}, {
+  id: 2,
+  name: "Mary's project"
+}]
+
+mag.module('dashboard', {
+  controller: function(props) {
+    this.selectedProject = mag.prop('...')
+    this.selectedUser = mag.prop('...')
+
+    this.usersFilter = mag.module('filter-users', filter, {
+      list: users,
+      binds: this.selectedUser
+    })
+
+  },
+  view: function(element, props, state) {
+
+    state.users = state.usersFilter
+
+    state.projects = mag.module('filter-projects', filter, {
+      list: projects,
+      binds: state.selectedProject
+    })
+  }
+})
