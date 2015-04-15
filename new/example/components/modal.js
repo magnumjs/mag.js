68dcbb0549520cc4f5b5de13084901f30860e703
magnumjs <magnumjs@users.noreply.github.com>
2015-04-14 22:21:34 -0400

    added html template

--- new/example/components/modal.js
+++ new/example/components/modal.js
@@ -1,35 +1,47 @@
-    var modal = {
-      controller: function(props) {
-        this.visible = mag.prop(props.visible || false)
-      },
-      view: function(element, props, state) {
-        if (state.visible()) {
-          element.classList.remove('hide')
-          state.wrapper = {
-            _html: props.content()
-          }
-        } else {
-          element.classList.add('hide')
-        }
-        state.button = {
-          _onclick: function() {
-            state.visible(false)
-          }
-        }
+/*
+<div id="modalCase">
+<div id="modal" class="hide">
+  <section class="modal">
+    <section class="wrapper">
+    </section>
+    <button class="close-modal">Close</button>
+  </section>
+</div>
+<button>Open</button>
+</div>
+*/
+var modal = {
+  controller: function(props) {
+    this.visible = mag.prop(props.visible || false)
+  },
+  view: function(element, props, state) {
+    if (state.visible()) {
+      element.classList.remove('hide')
+      state.wrapper = {
+        _html: props.content()
+      }
+    } else {
+      element.classList.add('hide')
+    }
+    state.button = {
+      _onclick: function() {
+        state.visible(false)
       }
     }
+  }
+}
 
-    mag.module('modalCase', {
-      view: function(element, props, state) {
-        state.button = {
-          _onclick: function() {
-            state.modal = mag.module('modal', modal, {
-              visible: true,
-              content: function() {
-                return '<b>HTML</b>'
-              }
-            })
+mag.module('modalCase', {
+  view: function(element, props, state) {
+    state.button = {
+      _onclick: function() {
+        state.modal = mag.module('modal', modal, {
+          visible: true,
+          content: function() {
+            return '<b>HTML</b>'
           }
-        }
+        })
       }
-    })
+    }
+  }
+})
