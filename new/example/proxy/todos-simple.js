88424eac3fb39662338913f08bdce603c9db9196
magnumjs <magnumjs@users.noreply.github.com>
2015-04-17 05:59:11 -0400

    updated with necessary code

--- new/example/proxy/todos-simple.js
+++ new/example/proxy/todos-simple.js
@@ -23,33 +23,31 @@ todo.controller = function(props) {
   this.todoList = props.todoList;
 
   this.button = {
-    _onclick: function(e) {
-      this.todoList = this.todoList.filter(function(x, i) {
+    _onclick: function() {
+      this.todoList = this.todoList.filter(function(x) {
         if (x.done) return false;
         return true;
       });
     }.bind(this)
   };
-
 };
 
 todo.view = function(element, props, state) {
 
   state.$todoList = {
     _config: function(node, isNew, context, index) {
-      node.children[0].setAttribute('index', index);
       node.children[0].checked = state.todoList[index].done;
     },
-    _onclick: function(e) {
-      var index = e.target.getAttribute('index');
-      state.todoList[index].done = e.target.checked;
+    _onclick: function(e, index) {
+      state.todoList[index].done = !state.todoList[index].done;
+      this.classList.toggle('done');
     }
   };
 
   state.add = {
     _onclick: function() {
       state.todoList.push({
-        todoText: state.todoInput, // for non Firefox - e.target.parentNode.children[0].value
+        todoText: state.todoInput, // for non Firefox - this|e.target.parentNode.children[0].value
         done: false
       });
       return false;
@@ -68,3 +66,4 @@ mag.module("todo", todo, props);
 // 1. list of todos
 // 2. add new todos
 // 3. remove todos
+// 4. toggle completed/done class
