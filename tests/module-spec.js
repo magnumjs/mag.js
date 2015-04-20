ec1f414bb656c62ee71ce41502aeddc4537a408a
magnumjs <magnumjs@users.noreply.github.com>
2015-04-20 17:28:58 -0400

    Update module-spec.js

--- tests/module-spec.js
+++ tests/module-spec.js
@@ -6,6 +6,31 @@ function afterDraw(expect) {
   });
 }
 
+
+var view = {
+  controller: function(p) {
+    this.p = [1, 2]
+    this.h2 = {
+      _text: 'tester',
+      _config: function(n, is, c, i) {
+        c.onunload = function() {
+          console.log('unloaded', arguments)
+        }
+      }
+    }
+  },
+  view: function(e, p, s) {
+
+    s.b = {
+      _onclick: function() {
+        s.p = [2]
+        s.h2 = null
+      }
+    }
+  }
+}
+
+
 // specs code
 describe("MagJS - module", function() {
 
@@ -25,22 +50,9 @@ describe("MagJS - module", function() {
 
   it("is accepts two arguments", function() {
     spyOn(mag, 'module').andCallThrough()
-
-    var view = {
-      controller: function(p) {
-        this.p = [1, 2]
-      },
-      view: function(e, p, s) {
-        s.h2 = 'tester'
-        s.b = {
-          _onclick: function() {
-            s.p = [2]
-          }
-        }
-      }
-    }
-
     mag.module('test', view)
+
+
     expect(mag.module).toHaveBeenCalledWith('test', view)
 
     afterDraw(function() {
@@ -52,4 +64,13 @@ describe("MagJS - module", function() {
       });
     });
   });
+
+  it("will call onunload after node is removed", function() {
+    mag.module('test', view)
+    afterDraw(function() {
+      $('#test b').click()
+      expect($('#test h2').text()).toEqual('')
+    })
+  })
+
 });
