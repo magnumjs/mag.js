ea554d4c57b6e3301b68be4d0504e9899431396b
magnumjs <magnumjs@users.noreply.github.com>
2015-04-20 15:18:22 -0400

    Update module-spec.js

--- tests/module-spec.js
+++ tests/module-spec.js
@@ -1,10 +1,19 @@
 
 // specs code
-describe("MagJS", function() {
+describe("MagJS - module", function() {
 
+beforeEach(function(){
+  affix("div#test h2")
+})
 
   it("is defined", function() {
     expect(mag.module).toBeDefined();
+  });
+  it("is accepts three argument", function() {
+    
+    spyOn(mag, 'module') 
+    mag.module('test')
+    expect(obj.method).toHaveBeenCalledWith('test')
   });
 
 });
