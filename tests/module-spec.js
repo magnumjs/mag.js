51eef3d4822c4524022e936edf778f0e73d658ad
magnumjs <magnumjs@users.noreply.github.com>
2015-04-20 15:54:19 -0400

    Update module-spec.js

--- tests/module-spec.js
+++ tests/module-spec.js
@@ -1,19 +1,37 @@
-
 // specs code
 describe("MagJS - module", function() {
 
-beforeEach(function(){
-  affix("div#test h2")
-})
+  beforeEach(function() {
+    affix("div#test h2+p>b")
+  })
 
   it("is defined", function() {
     expect(mag.module).toBeDefined();
   });
-  it("is accepts three argument", function() {
-    
-    spyOn(mag, 'module') 
+  it("is accepts one argument", function() {
+
+    spyOn(mag, 'module')
     mag.module('test')
     expect(mag.module).toHaveBeenCalledWith('test')
+  });
+
+  it("is accepts two arguments", function() {
+    var view = {
+      view: function(e, p, s) {
+        s.h2 = 'tester'
+      }
+    }
+    spyOn(mag, 'module').andCallThrough()
+
+    mag.module('test', view)
+    expect(mag.module).toHaveBeenCalledWith('test', view)
+
+    waits(0)
+
+    runs(function() {
+      expect($('#test h2').text()).toEqual('tester')
+    });
+
   });
 
 });
