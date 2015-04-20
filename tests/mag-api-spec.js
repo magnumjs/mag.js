ecce29ce0af806adb9930e54cb63df5694af66bd
magnumjs <magnumjs@users.noreply.github.com>
2015-04-20 15:13:12 -0400

    Update mag-api-spec.js

--- tests/mag-api-spec.js
+++ tests/mag-api-spec.js
@@ -6,5 +6,14 @@ describe("MagJS", function() {
   it("is defined", function() {
     expect(mag).toBeDefined();
   });
+  
+  it("has public methods",function(){
+        expect(mag.module).toBeDefined();
+        expect(mag.hookin).toBeDefined();
+        expect(mag.draw).toBeDefined();
+        expect(mag.prop).toBeDefined();
+        expect(mag.withProp).toBeDefined();
+
+  })
 
 });
