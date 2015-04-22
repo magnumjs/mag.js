e645891864599be978089487cdb3bda73afa5e5e
magnumjs <magnumjs@users.noreply.github.com>
2015-04-22 13:08:58 -0400

    Update module.js
    concat to local var - was increasing size of args ad infinitum

--- src/module.js
+++ src/module.js
@@ -5,8 +5,8 @@
 
   var mod = {
     modules: [],
-    promises:[],
-    deferreds:[],
+    promises: [],
+    deferreds: [],
     controllers: [],
     elements: []
   }
@@ -16,36 +16,36 @@
 
     // FireFox support only
     // Removing since we are now native with Object.observe which is only Chrome & Opera
-    
+
     if (typeof Proxy !== 'undefined') {
       controller = new Proxy(new mod.controller, {
         get: function(target, prop) {
           if (target[prop] === undefined && ['watchers', 'toJSON', 'called', 'onload', 'onunload'].indexOf(prop) === -1) {
             var a = fill.find(element, prop),
               greedy = prop[0] === '$',
-            v, // can be an array, object or string
-            // for each
-            tmp = []
-            a.forEach(function(item, index) {
-              if (a[index]) {
-                if (a[index].value && a[index].value.length > 0) {
-                  v = a[index].value
-                  if (a[index].type && (a[index].type == 'checkbox' || a[index].type == 'radio')) {
-                    v = {
-                      _text: v,
-                      _checked: a[index].checked
+              v, // can be an array, object or string
+              // for each
+              tmp = []
+              a.forEach(function(item, index) {
+                if (a[index]) {
+                  if (a[index].value && a[index].value.length > 0) {
+                    v = a[index].value
+                    if (a[index].type && (a[index].type == 'checkbox' || a[index].type == 'radio')) {
+                      v = {
+                        _text: v,
+                        _checked: a[index].checked
+                      }
+                      tmp.push(v)
                     }
-                    tmp.push(v)
+                  } else if (a[index].innerText && a[index].innerText.length > 0) {
+                    v = a[index].innerText
+                  } else if (a[0].innerHTML && a[index].innerHTML.length > 0) {
+                    v = a[index].innerHTML
                   }
-                } else if (a[index].innerText && a[index].innerText.length > 0) {
-                  v = a[index].innerText
-                } else if (a[0].innerHTML && a[index].innerHTML.length > 0) {
-                  v = a[index].innerHTML
                 }
-              }
-            })
-            if (tmp.length > 0 && greedy) return tmp
-            return v
+              })
+              if (tmp.length > 0 && greedy) return tmp
+              return v
           }
           return target[prop]
         }
@@ -53,22 +53,25 @@
     } else {
       controller = new mod.controller
     }
-    
+
     //controller = new mod.controller
     return controller
   }
 
   mod.submodule = function(module, args) {
+
     var controller = function(args) {
       return (module.controller || function() {}).apply(this, args)
     }.bind({}, args)
 
-    var view = function(ctrl) {
-      if (arguments.length > 1) args = args.concat([].slice.call(arguments, 1))
-      var template = module.view.apply(module, args ? [ctrl].concat(args) : [ctrl])
+    var view = function(ctrl, ele) {
+
+      if (arguments.length > 1) var nargs = args.concat([].slice.call(arguments, 1))
+      var template = module.view.apply(module, nargs ? [ctrl].concat(nargs) : [ctrl])
+
       if (args[0] && args[0].key != null) template.attrs.key = args[0].key
       // There's no template return ??
-      return template
+      //return template
     }
     controller.$original = module.controller
 
@@ -84,6 +87,8 @@
 
   mod.getArgs = function(i) {
     var args = mod.modules[i].controller && mod.modules[i].controller.$$args ? [mod.controllers[i]].concat(mod.modules[i].controller.$$args) : [mod.controllers[i]]
+    // args that contaian circular references will throw an exception up the chain
+    // TODO: filter for circual references such as DOM nodes
     return args
   }
   mag.mod = mod
