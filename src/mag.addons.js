07028aa0155896903d897ba880ec9eb7ccc26c91
magnumjs <magnumjs@users.noreply.github.com>
2015-04-23 23:51:22 -0400

    Update mag.addons.js
    Added callback after onchange event and more events

--- src/mag.addons.js
+++ src/mag.addons.js
@@ -3,55 +3,58 @@ Mag.JS AddOns v0.1.2
 (c) Michael Glazer
 https://github.com/magnumjs/mag.js/new/master/new
 */
-mag.addons={};
+mag.addons = {};
 // helper function for non proxy supported browser i.e. NOT firefox
 // state.fom = mag.addons.binds(state)
-mag.addons.binds=function(data, attachTo) {
-  var addThis= {
-    _onchange: function(e) {
-      data[e.target.name] = e.target.type == 'checkbox' ? e.target.checked : e.target.value
+mag.addons.binds = function(data, attachTo, callback) {
+    var handler = function(e) {
+        data[e.target.name] = e.target.type == 'checkbox' ? e.target.checked : e.target.value
+        callback()
     }
-  }
-  if(attachTo) mag.addons.merge(addThis,attachTo)
-return addThis
+    var addThis = {}
+
+    var events = ['_onchange', '_oninput']
+    for (var k in events) addThis[k] = handler
+    if (attachTo) mag.addons.merge(addThis, attachTo)
+    return addThis
 };
 
-mag.addons.merge=function(source, destination){
-  for(var k in source) destination[k] =source[k]
+mag.addons.merge = function(source, destination) {
+    for (var k in source) destination[k] = source[k]
 }
 
 // show hide
 
 mag.addons.show = function(condition) {
-  return {
-    _config: function(n) {
-      if (!condition) n.style.display = 'none';
-      else n.style.display = 'block';
-    }
-  };
+    return {
+        _config: function(n) {
+            if (!condition) n.style.display = 'none';
+            else n.style.display = 'block';
+        }
+    };
 }
 
 mag.addons.hide = function(condition) {
-  return {
-    _config: function(n) {
-      if (condition) n.style.display = 'none';
+    return {
+        _config: function(n) {
+            if (condition) n.style.display = 'none';
+        }
     }
-  }
 }
 
 
 mag.addons.onload = function(element) {
-  element.classList.remove("hide")
+    element.classList.remove("hide")
 };
 
 
 // hookin
 
 mag.hookin('attributes', 'className', function(data) {
-  if(!data.node.classList.contains(data.value)){
-    data.value = data.node.classList.length>0 ? data.node.classList + ' ' + data.value : data.value
-  }
-  // same thing
-  //data.node.classList.add(data.value)
-  data.key = 'class'
+    if (!data.node.classList.contains(data.value)) {
+        data.value = data.node.classList.length > 0 ? data.node.classList + ' ' + data.value : data.value
+    }
+    // same thing
+    //data.node.classList.add(data.value)
+    data.key = 'class'
 })
