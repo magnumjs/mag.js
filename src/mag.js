9d3251e12b47d16c1965294f1eec4c181c4c9550
magnumjs <magnumjs@users.noreply.github.com>
2015-04-30 08:37:14 -0400

    Update mag.js
    new force redraw option to clear cache
    toJson for mag prop on dom elements
    better error messages

--- src/mag.js
+++ src/mag.js
@@ -14,14 +14,14 @@
   mag.running = false
 
   var redrawing = false
-  mag.redraw = function() {
+  mag.redraw = function(force) {
     if (redrawing) {
       // do we ever get here?
       // necessary?
       return
     }
     redrawing = true
-    render.redraw(module || render.module || {}, fill)
+    render.redraw(module || render.module || {}, fill, force)
     redrawing = false
   }
 
@@ -58,6 +58,11 @@
 
     prop.toJSON = function() {
       // return a copy
+      if(store && store.nodeType){
+        //make sure no circular references
+      
+        return fill.elementToObject(store)
+      }
       return store
     }
 
@@ -75,7 +80,7 @@
 
   var hookins = {
     attributes: [],
-    elementMatcher : []
+    elementMatcher: []
   }
   mag.hookin = function(name, key, handler) {
     hookins[name].push({
@@ -105,6 +110,7 @@
     }
   }
 
+  var topComponent
   mag.module = function(domElementId, moduleObject, props, clone) {
 
     var index = render.roots.indexOf(domElementId)
@@ -121,13 +127,13 @@
 
     //DOM
     var element = document.getElementById(domElementId)
-    if (!element) throw Error('Mag.JS Module - invalid node')
+    if (!element) throw Error('Mag.JS Module - invalid node id: ' + domElementId)
 
 
     render.roots[index] = element.id
 
     //MODULE
-    if (!moduleObject.view) throw Error('Mag.JS module - requires a view')
+    if (!moduleObject.view) throw Error('Mag.JS module - requires a view: ' + JSON.stringify(moduleObject))
 
     // TODO: should props be frozen or changeable?
     var mod = module.submodule(moduleObject, [Object.freeze(props || {})])
@@ -135,15 +141,17 @@
     var controller = module.getController(mod, element, fill)
 
     module.controllers[index] = controller
-    
+
     if (controller.onunload) render.unloaders.push({
       controller: controller,
       handler: controller.onunload
     })
 
-    module.modules[index] = mod
-    module.elements[index] = clone ? element.cloneNode(true) : element
 
+    module.modules[index] = mod
+    
+    module.elements[index] = clone ? element.cloneNode(true): element
+    module.elements[index].cloner = clone
 
     module.promises[index] = new Promise(function(resolve, reject) {
       module.deferreds[index] = arguments
@@ -163,8 +171,20 @@
     // interpolations haven't occurred yet
     // return a promise in a settergetter
 
+
+    // return {
+    //   promise: module.promises[index],
+    //   html :{_html: fill.cloneNodeWithEvents(module.elements[index]) }
+    // }
+
+// return propify(module.promises[index], {
+//       _html: module.elements[index].innerHTML
+//     })
+
     return propify(module.promises[index], {
-      _html: module.elements[index].innerHTML
+     // _html: fill.cloneNodeWithEvents(module.elements[index])
+     _html : mag.prop(module.elements[index] )
+      // _html: function(){ return module.elements[index] }
     })
 
   }
