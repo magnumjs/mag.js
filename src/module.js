8f6dbe96d86d36887d45eb235431fb381ddb147a
magnumjs <magnumjs@users.noreply.github.com>
2015-08-29 17:14:33 -0400

    Update module.js
    New auto getter for user input fields

--- src/module.js
+++ src/module.js
@@ -29,26 +29,26 @@
               v, // can be an array, object or string
               // for each
               tmp = []
-              a.forEach(function(item, index) {
-                if (a[index]) {
-                  if (a[index].value && a[index].value.length > 0) {
-                    v = a[index].value
-                    if (a[index].type && (a[index].type == 'checkbox' || a[index].type == 'radio')) {
-                      v = {
-                        _text: v,
-                        _checked: a[index].checked
-                      }
-                      tmp.push(v)
+            a.forEach(function(item, index) {
+              if (a[index]) {
+                if (a[index].value && a[index].value.length > 0) {
+                  v = a[index].value
+                  if (a[index].type && (a[index].type == 'checkbox' || a[index].type == 'radio')) {
+                    v = {
+                      _text: v,
+                      _checked: a[index].checked
                     }
-                  } else if (a[index].innerText && a[index].innerText.length > 0) {
-                    v = a[index].innerText
-                  } else if (a[0].innerHTML && a[index].innerHTML.length > 0) {
-                    v = a[index].innerHTML
+                    tmp.push(v)
                   }
+                } else if (a[index].innerText && a[index].innerText.length > 0) {
+                  v = a[index].innerText
+                } else if (a[0].innerHTML && a[index].innerHTML.length > 0) {
+                  v = a[index].innerHTML
                 }
-              })
-              if (tmp.length > 0 && greedy) return tmp
-              return v
+              }
+            })
+            if (tmp.length > 0 && greedy) return tmp
+            return v
           }
           return target[prop]
         }
@@ -84,31 +84,74 @@
 
 
     var controller = function() {
-      return (module.controller || function() {}).apply(this, args) || this
-    },
+        return (module.controller || function() {}).apply(this, args) || this
+      },
       view = function(ctrl, ele) {
 
         if (arguments.length > 1) var nargs = args.concat([].slice.call(arguments, 1))
         module.view.apply(module, nargs ? [ctrl].concat(nargs) : [ctrl])
 
-      }, output = {
+      },
+      output = {
         controller: controller,
         view: view
       }
       // why do we need these?
 
-      //controller.$original = module.controller
-      //view.$original = module.view
+    //controller.$original = module.controller
+    //view.$original = module.view
     controller.$$args = args
     return output
   }
 
   mod.getArgs = function(i) {
     var args = mod.modules[i] && mod.modules[i].controller && mod.modules[i].controller.$$args ? [mod.controllers[i]].concat(mod.modules[i].controller.$$args) : [mod.controllers[i]]
-    // args that contaian circular references will throw an exception up the chain
+      // args that contaian circular references will throw an exception up the chain
+
+    attachToArgs(i, args, this.elements[i]);
 
     return args
   }
+
+  var added = [];
+
+  var attacher = function(i, k, obj, element) {
+    var oval = obj[k];
+    // only for user input fields
+    var found = mag.fill.find(element, k);
+
+    if (['INPUT', 'SELECT', 'TEXTAREA'].indexOf(found[0].tagName) === -1) return;
+
+    Object.defineProperty(obj, k, {
+      get: function() {
+        var found = mag.fill.find(element, k);
+        if (found[0].value && found[0].value !== oval) {
+          return found[0].value;
+        }
+        return oval;
+      }
+    });
+  };
+
+  function attachToArgs(i, args, element) {
+
+    // for each property set accessor property
+
+    // get notification when accessed 
+
+    // call fill to get and assign
+
+    if (!added[i]) added[i] = [];
+
+    for (var k in args[0]) {
+      if (!added[i][k]) {
+        attacher(i, k, args[0], element);
+        added[i][k] = true;
+      }
+    }
+  }
+
+
   mag.mod = mod
 
 })(window.mag || {})
