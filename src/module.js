a6398d89a22edd088a33909be4149a1856981f74
magnumjs <magnumjs@users.noreply.github.com>
2015-09-08 14:56:45 -0400

    Update module.js
    version 0.13+ Check for object _value

--- src/module.js
+++ src/module.js
@@ -114,35 +114,31 @@
   }
 
   var added = [];
-  var getParent = function(parts, parentElement) {
-    for (var i = 1; i < parts.length; i += 2) {
-      var key = parts[i];
-      var index = parts[i + 1];
-      parentElement = mag.fill.find(parentElement, key);
-      // console.log(key, index, parentElement[index])
-      parentElement = parentElement[index];
-    }
-    return parentElement;
+  var getParent=function(parts, parentElement){
+        for (var i=1;i<parts.length; i+=2){
+          var key = parts[i];
+          var index = parts[i+1];
+          parentElement = mag.fill.find(parentElement, key);
+          // console.log(key, index, parentElement[index])
+          parentElement = parentElement[index];
+      }
+      return parentElement;
   };
-  var getElement = function(obj, k, i, parentElement) {
+  var getElement =function (obj, k, i, parentElement) {
 
     // search within _key if there
+    var parts = i.toString().split('-'), found;
 
-    var parts = i.toString().split('-'),
-      found;
-
-    if (parts.length >= 3) {
+    if (parts.length >= 3){
       // recurse
       parentElement = getParent(parts, parentElement)
-      found = mag.fill.find(parentElement, k);
+      found = mag.fill.find( parentElement, k);
 
     } else {
 
-      var last = parseInt(parts.pop()),
-        index = !isNaN(last) ? last : 0;
+      var last = parseInt(parts.pop()), index = !isNaN(last) ? last : 0;
       found = mag.fill.find(parentElement[index] ? parentElement[index] : parentElement, k);
     }
-
     // first user input field
     var founder = isInput(found);
     if (founder && !founder.eventOnFocus) {
@@ -170,16 +166,20 @@
   }
   var attacher = function(i, k, obj, element) {
     var oval = obj[k];
+    // if k =='_value' use parent
+    if(k==='_value') k = i.split('-').pop();
+
     // only for user input fields
     var found = mag.fill.find(element, k);
     var founder = isInput(found);
     if (typeof oval !== 'function' && founder) {
 
       var founderCall = getElement.bind({}, obj, k, i, element);
-
+      founderCall();
       Object.defineProperty(obj, k, {
         get: function() {
           var founder = founderCall();
+          
           // set on focus listener once
           if (founder && founder.value !== 'undefined' && founder.classList.contains('mag-dirty') && founder.value !== oval) {
             oval = founder.value;
