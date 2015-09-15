f8c684b9cce9947cfa8fd1d9077f1456d258041c
magnumjs <magnumjs@users.noreply.github.com>
2015-09-14 22:04:29 -0400

    Update module.js
    version 0.15.1 - parent info available to sub modules via props._parent which comes from and is stored in the modules view object view._info

--- src/module.js
+++ src/module.js
@@ -3,6 +3,8 @@
 
   "use strict";
 
+
+
   var mod = {
     modules: [],
     promises: [],
@@ -89,7 +91,7 @@
       },
       view = function(ctrl, ele) {
         // container element available to sub components
-        
+        module.view._info = {node:ele}
         if (arguments.length > 1) var nargs = args.concat([].slice.call(arguments, 1))
         module.view.apply(module, nargs ? [ctrl].concat(nargs) : [ctrl])
       },
