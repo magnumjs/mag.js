3044b0a68d24017f89372cfd3637bcd6739f2e79
magnumjs <magnumjs@users.noreply.github.com>
2015-09-17 15:36:27 -0400

    Update mag.utils.js
    Added events

--- src/addons/mag.utils.js
+++ src/addons/mag.utils.js
@@ -15,6 +15,55 @@ mag.utils ={}
 Common reusable minimal Interfaces you can implement
 */
 
+/****** UTILS EVENTS ******/
+
+/*
+mag.utils.events.publish('/page/load', {
+	url: '/some/url/path' // any argument
+});
+
+var subscription = mag.utils.events.subscribe('/page/load', function(obj) {
+	// Do something now that the event has occurred
+});
+
+// ...sometime later where I no longer want subscription...
+subscription.remove();
+*/
+
+mag.utils.events = function(){
+  var topics = {};
+  var hOP = topics.hasOwnProperty;
+
+  return {
+    subscribe: function(topic, listener) {
+      // Create the topic's object if not yet created
+      if(!hOP.call(topics, topic)) topics[topic] = [];
+
+      // Add the listener to queue
+      var index = topics[topic].push(listener) -1;
+
+      // Provide handle back for removal of topic
+      return {
+        remove: function() {
+          delete topics[topic][index];
+        }
+      };
+    },
+    publish: function(topic, info) {
+      // If the topic doesn't exist, or there's no listeners in queue, just leave
+      if(!hOP.call(topics, topic)) return;
+
+      // Cycle through topics queue, fire!
+      topics[topic].forEach(function(item) {
+      		item(info || {});
+      });
+    }
+  };
+})
+
+/****** UTILS EVENT ******/
+
+
 /****** UTILS COLLECTIONS ******/
 
 mag.utils.collection = function (a) {
