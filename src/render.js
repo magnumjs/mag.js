5395bdd53a08857ad5097d80721279d26cc84837
magnumjs <magnumjs@users.noreply.github.com>
2015-10-11 19:35:32 -0400

    Update render.js
    v0.21 - checkbox support for auto wiring elements requires a default value e.g. state.mycheckbox = {_value:''}

--- src/render.js
+++ src/render.js
@@ -1,5 +1,5 @@
 /*
-MagJS v0.20
+MagJS v0.21
 http://github.com/magnumjs/mag.js
 (c) Michael Glazer
 License: MIT
@@ -84,19 +84,29 @@ License: MIT
         index = !isNaN(last) ? last : 0;
       found = mag.fill.find(parentElement[index] ? parentElement[index] : parentElement, k);
     }
+
     // first user input field
     var founder = isInput(found);
     if (founder && !founder.eventOnFocus) {
 
-      var onfocus = function() {
+      var onit = function(parent, event) {
+
         this[MAGNUM] = this[MAGNUM] || {}
         if (!this[MAGNUM].dirty) {
           this[MAGNUM].dirty = 1
         }
-      }
+        
+        if(['radio','checkbox'].indexOf(this.type)>-1){
+          obj['_checked'] = this.checked;
+          var idx =  mag.utils.items.getItem(parent.id)
+          mag.redraw(parent, idx, 1)
+        }
+      }.bind(founder, parentElement)
 
-      founder.addEventListener("focus", onfocus, false);
-      founder.eventOnFocus = true;
+      founder.addEventListener("click", onit, false);
+      founder.addEventListener("focus", onit, false);
+
+      founder.eventOnFocus = 1;
     }
     return founder;
   }
@@ -120,19 +130,22 @@ License: MIT
     // only for user input fields
     var found = mag.fill.find(element, k);
     var founder = isInput(found);
-    if (typeof oval !== 'function' && founder) {
 
+    if (typeof oval !== 'function' && founder) {
       var founderCall = getElement.bind({}, obj, k, i, element);
       founderCall();
+      
+      
       Object.defineProperty(obj, k, {
         configurable: true,
         get: function() {
           var founder = founderCall();
-
           // set on focus listener once
           if (founder && founder.value !== 'undefined' && (founder[MAGNUM] && founder[MAGNUM].dirty) && founder.value !== oval) {
+
             oval = founder.value;
             mag.redraw(element, i, 1)
+            
             return founder.value;
           }
           return oval;
@@ -141,9 +154,9 @@ License: MIT
           var founder = founderCall();
 
           if (founder && founder.value !== 'undefined' && founder.value !== newValue && newValue !== oval) {
+
             founder.value = newValue;
             oval = newValue;
-
 
           }
         }
