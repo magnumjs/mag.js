f5e3399080eaf17354fcc62b54cfb8f102381c14
magnumjs <magnumjs@users.noreply.github.com>
2015-10-17 09:02:18 -0400

    Update mag.addons.0.2.js

--- src/mag.addons.0.2.js
+++ src/mag.addons.0.2.js
@@ -194,6 +194,61 @@ Requires: MagJS (core) Addons: Ajax , Router
   }
 
 
+
+//ROUTER:
+
+
+mag.route = (function (window) {
+
+    'use strict';
+
+    var routes = [];
+
+    function addRoute(route, handler) {
+
+        routes.push({parts: route.split('/'), handler: handler});
+    }
+
+    function load(route) {
+        window.location.hash = route;
+    }
+
+    function start() {
+
+        var path = window.location.hash.substr(1),
+            parts = path.split('/'),
+            partsLength = parts.length;
+
+        for (var i = 0; i < routes.length; i++) {
+            var route = routes[i];
+            if (route.parts.length === partsLength) {
+                var params = [];
+                for (var j = 0; j < partsLength; j++) {
+                    if (route.parts[j].substr(0, 1) === ':') {
+                        params.push(parts[j]);
+                    } else if (route.parts[j] !== parts[j]) {
+                        break;
+                    }
+                }
+                if (j === partsLength) {
+                    route.handler.apply(undefined, params);
+                    return;
+                }
+            }
+        }
+    }
+
+    window.onhashchange = start;
+
+    return {
+        addRoute: addRoute,
+        load: load,
+        start: start
+    };
+
+}(window));
+
+
   //PLUGINS!
   // hookins
 
@@ -231,6 +286,11 @@ Requires: MagJS (core) Addons: Ajax , Router
 
     data.value = data.value.trim()
   })
+
+
+
+
+
 
 /** DEPRECATED - avoid use **/
 
