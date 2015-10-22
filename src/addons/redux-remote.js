fdd31887a4f81c158d511bd6d286c3672ffe47e4
magnumjs <magnumjs@users.noreply.github.com>
2015-10-22 15:34:02 -0400

    Update redux-remote.js
    removed required middleware

--- src/addons/redux-remote.js
+++ src/addons/redux-remote.js
@@ -5,51 +5,6 @@ http://github.com/magnumjs/mag.js
 License: MIT
 */
 
-//TODO: put this somewhere else
-// e.g. it's own middleware file
-
-Redux.readyStatePromise = function(store) {
-  return function(next) {
-    return function(action) {
-
-      if (!action.promise) {
-        return next(action);
-      }
-
-      function makeAction(ready, data) {
-        var newAction = Object.assign({}, action, {
-          ready: ready
-        }, data);
-        delete newAction.promise;
-        return newAction;
-      }
-
-      var SUCCESS = FAILURE = REQUEST = action.type;
-
-      if (action.types && Array.isArray(action.types)) {
-        REQUEST = action.types[0];
-        SUCCESS = action.types[1];
-        FAILURE = action.types[2];
-      }
-
-      next(makeAction(false, {
-        type: REQUEST
-      }));
-      return action.promise.then(function(result) {
-        return next(makeAction(true, {
-          result: result,
-          type: SUCCESS
-        }));
-      }, function(error) {
-        return next(makeAction(true, {
-          error: error,
-          type: FAILURE
-        }));
-      });
-    };
-  };
-};
-
 
 //TODO: add more action types for hookins from user
 // e.g. REMOTE_LOAD_FAILED, REMOTE_SAVE_SUCCESS, REMOTE_SAVE_PENDING ..
