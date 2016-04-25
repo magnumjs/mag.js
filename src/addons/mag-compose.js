283a08f013ec098bc7bf0338767a6e61ff2c8b82
magnumjs <magnumjs@users.noreply.github.com>
2016-04-24 23:52:21 -0400

    Update mag-compose.js
    v0.1.4 - use mag.redraw to repaint when only props changed

--- src/addons/mag-compose.js
+++ src/addons/mag-compose.js
@@ -1,5 +1,5 @@
 /*
-Name: mag-compose v0.1.3
+Name: mag-compose v0.1.4
 Description: side loading props based on react-komposer
 Author: Michael Glazer
 License: MIT
@@ -21,12 +21,18 @@ mag.compose = function(handlerFunc) {
     var orig = new(component.controller || mag.noop)();
     var view = component.view || mag.noop;
     var LOADINGID = performance.now();
-    var loadingHtml = '<div id="' + LOADINGID + '">Loading ...</div>';
+    var ERRORID = performance.now();
+    var loadingHtml = '<div class="' + LOADINGID + '">Loading ...</div>';
+    var errorHtml = '<div class="' + ERRORID + '">Error: <error></error></div>';
+
     var cleanup, loading = true,
+      iserror = false,
       mod,
       error;
 
-
+    var _isError = function() {
+      return iserror;
+    };
     var _isLoading = function() {
       return loading;
     };
@@ -43,12 +49,16 @@ mag.compose = function(handlerFunc) {
 
         if (newestProps instanceof Error) {
           //same as above
-        }
+          iserror = true;
+          //console.log('Error!', newestProps)
+        } else {
 
-        // foreach clones update for id
-        mag.merge(newProps, newestProps);
-        // attach to object
-        mag.merge(mod.getProps(), newProps);
+          // foreach clones update for id
+          // mag.merge(newProps, newestProps);
+          // attach to object
+          mag.merge(mod.getProps(), newestProps);
+          mod.draw(1);
+        }
       });
 
     };
@@ -64,13 +74,19 @@ mag.compose = function(handlerFunc) {
 
         cleaners[clone.instanceId] = handlerFunc(newProps, function(id, newestProps) {
 
-          // if change from previous then force draw
-          mod.getState(id).$$i = performance.now();
+          if (newestProps instanceof Error) {
+            //same as above
+            iserror = true;
+            console.log('ErrorME!', newestProps)
+          } else {
 
-          // attach to object
-          mag.merge(newProps, newestProps);
-          // attach to object
-          mag.merge(mod.getProps(id), newProps);
+            // if change from previous then force draw
+
+            // attach to object
+            mag.merge(mod.getProps(id), newestProps);
+
+            mag.redraw(mag.getNode(mag.getId(id)), id, 1)
+          }
         }.bind({}, clone.instanceId));
 
       });
@@ -85,15 +101,24 @@ mag.compose = function(handlerFunc) {
 
       var prevProps;
       this.willload = function(e, node, newProps) {
-        // if (_isLoading()) {
-        //   node.insertAdjacentHTML('afterbegin', loadingHtml);
-        // }
+        if (_isLoading()) {
+          // node.insertAdjacentHTML('afterbegin', loadingHtml);
+          // node.insertAdjacentHTML('afterbegin', errorHtml);
+          // hide error
+          // mag.show(false, this[ERRORID] = {});
+        }
         _subscribe(newProps);
         if (orig.willload) orig.willload.apply(this, arguments);
       };
 
       this.willupdate = function(e, node, newProps) {
-        _subscribe(newProps);
+
+        if (_isError()) {
+          // show once
+
+        }
+
+        //_subscribe(newProps);
         _runClones();
 
         if (JSON.stringify(prevProps) === JSON.stringify(newProps)) {
@@ -107,13 +132,15 @@ mag.compose = function(handlerFunc) {
 
     };
 
-    // component.view = function(state) {
-
-    //   if (!_isLoading()) {
-    //     state[LOADINGID] = null;
-    //   }
-    //   view.apply(this, arguments);
-    // }
+    component.view = function(state) {
+      if (!_isLoading()) {
+        // mag.show(false, state[LOADINGID] = {});
+      }
+      if (_isError()) {
+        // mag.show(true, state[ERRORID] = {});
+      }
+      view.apply(this, arguments);
+    }
     mod = mag.module(id, component, props);
 
     return mod;
