43f8e985251dfd4b7222348c4416aba198eeab8d
magnumjs <magnumjs@users.noreply.github.com>
2017-01-13 10:46:25 -0500

    Update mag-cloak.js

--- src/addons/mag-cloak.js
+++ src/addons/mag-cloak.js
@@ -1,17 +1,40 @@
-//Example: http://jsbin.com/yepaqewucu/edit?html,js,output 
+/**
+ * There are various use cases for hidng content on load
+ * 1. on initial load to hide placeholder strings that will be interpolated
+ * 2. large data set load that might change and or flicker the content when loaded
+ * 3. async data loading, loading icon and reloading icon
+ */
+
+//Example: http://jsbin.com/kocodalaqu/edit?html,js,output 
 
 //Example with large data set: http://jsbin.com/muxisecaxa/edit?html,js,output
 
 /* MagJS Example - mag.cloak */
 
 mag.cloak = function(id, key) {
-
+  
+  var classHide =mag.cloak.classHideName= 'cloak';
+  
   var cloak = function(node, index) {
-
-    node.style.display = 'none';
-
+    
+    // should be already hidden at this point before code is run on page load class="cloak|hide"
+    //node.style.display = 'none';
+    //TODO: only onload? run only once then remove handler?
+    
     mag.utils.onLCEvent('willupdate', index, function(vnode) {
-      if (vnode.style.display == 'none') vnode.style.display = 'block'
+     //Check self
+     if(vnode.classList.contains(classHide)){
+       vnode.classList.remove(classHide)
+     }
+      //class based for all sub nodes
+      var nodes = vnode.getElementsByClassName(classHide);
+      if (nodes.length) {
+        for (var i = 0; i < nodes.length; i++) {
+          var item = nodes[i];
+          item.classList.remove(classHide);
+        }
+      }
+      //if (vnode.style.display == 'none') vnode.style.display = 'block'
     }.bind({}, node));
 
   };
