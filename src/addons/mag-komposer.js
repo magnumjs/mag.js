b084b29a8b1fada3e00973743c7faa2d953af2e6
magnumjs <magnumjs@users.noreply.github.com>
2017-06-29 12:15:16 -0400

    Update mag-komposer.js

--- src/addons/mag-komposer.js
+++ src/addons/mag-komposer.js
@@ -45,7 +45,7 @@ mag.komposer = function(handlerFunc, loadingComp, errorComp) {
       cleaners[props.key] = handlerFunc(props, function(key, ids, nprops) {
 
         loading[key] = false;
-        var cp = composed().getProps(ids);
+        var cp = mag.mod.getProps(ids);
 
         if (nprops instanceof Error) {
           errors[key] = true;
